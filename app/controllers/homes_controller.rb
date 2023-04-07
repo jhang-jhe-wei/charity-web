e36af52dc91f45a298da1b751ca4208fc1daa2c5
wells <jhang0912407249@gmail.com>
2023-04-07 19:34:16 +0800

    Refactor: match rubocop lint

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,13 +1,11 @@
+# frozen_string_literal: true
+
 class HomesController < ApplicationController
-  def recommend
-  end
+  def recommend; end
 
-  def my_favorite
-  end
+  def my_favorite; end
 
-  def fan_page
-  end
+  def fan_page; end
 
-  def volunteer_journey
-  end
+  def volunteer_journey; end
 end
--- app/controllers/notifies_controller.rb
+++ app/controllers/notifies_controller.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 class NotifiesController < ApplicationController
   def index
     @auth_link = LineNotify.get_auth_link(current_user.line_id)
   end
 
-  def tutorial
-  end
+  def tutorial; end
 end
