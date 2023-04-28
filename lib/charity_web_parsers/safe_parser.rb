87c46ff5abdb337e250abc1f1291aaf03f3321fc
wells <jhang0912407249@gmail.com>
2023-04-28 20:10:19 +0800

    feat: get event detail from www.safe.org.tw

--- lib/charity_web_parsers/safe_parser.rb
+++ lib/charity_web_parsers/safe_parser.rb
@@ -18,8 +18,6 @@ class SafeParser
       img_url = item.at_css('.Img img')&.[]('src')
       name = item.at_css('.title a')&.text&.strip
       organizer = item.at_css('.classTitle')&.text
-      location = item.at_css('.location')&.text
-      time = item.at_css('.date')&.text
       event_type = item.at_css('.category')&.text
       working_type = item.at_css('.workingType')&.text
       bonus = item.at_css('.bonus')&.text
@@ -29,22 +27,28 @@ class SafeParser
       link = item.at_css('.title a')&.[]('href')
       source_type = SOURCE_TYPE
 
-      data << {
-        img_url: img_url,
-        name: name,
-        organizer: organizer,
-        location: location,
-        time: time,
-        event_type: event_type,
-        working_type: working_type,
-        bonus: bonus,
-        viewer: viewer,
-        remark: remark,
-        deadline: deadline,
-        link: link,
-        source_type: source_type
-      }
+      detail_doc = Nokogiri::HTML(Faraday.get('https://www.safe.org.tw/volunteer-detail/55__6/').body)
+      location = /服務地點:\s*(.*)/.match(detail_doc.at('.textEditor ul li:first-child').text.strip)[1]
+      time = detail_doc.at('.textEditor ul li:nth-child(2)').text.strip.sub('服務時間:',
+                                                                            '').strip || item.at_css('.date')&.text&.sub('期間: ', '')
+
+      data << ({
+        img_url:,
+        name:,
+        organizer:,
+        location:,
+        time:,
+        event_type:,
+        working_type:,
+        bonus:,
+        viewer:,
+        remark:,
+        deadline:,
+        link:,
+        source_type:
+      })
     end
+    Rails.logger.debug data
     data
   end
 end
