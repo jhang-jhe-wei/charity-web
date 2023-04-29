4dfeb82a37a11a0442afbd7756e0bd36c5e00e1b
wells <jhang0912407249@gmail.com>
2023-04-29 21:17:18 +0800

    feat: return N/A when attr blank

--- app/models/charitable_event.rb
+++ app/models/charitable_event.rb
@@ -4,4 +4,44 @@ class CharitableEvent < ApplicationRecord
   store :extra_infos,
         accessors: %i[img_url name organizer location time event_type working_type bonus viewer remark deadline link
                       source_type]
+
+  def name
+    super.presence || 'N/A'
+  end
+
+  def organizer
+    super.presence || 'N/A'
+  end
+
+  def location
+    super.presence || 'N/A'
+  end
+
+  def time
+    super.presence || 'N/A'
+  end
+
+  def event_type
+    super.presence || 'N/A'
+  end
+
+  def working_type
+    super.presence || 'N/A'
+  end
+
+  def bonus
+    super.presence || 'N/A'
+  end
+
+  def viewer
+    super.presence || 'N/A'
+  end
+
+  def remark
+    super.presence || 'N/A'
+  end
+
+  def deadline
+    super.presence || 'N/A'
+  end
 end
