145334c83aa52ba14e041470bb209e206eb9498f
wells <jhang0912407249@gmail.com>
2023-05-01 16:45:03 +0800

    fix: lose some columns in charitable events

--- db/migrate/20230501050020_add_columns_to_charitable_events.rb
+++ db/migrate/20230501050020_add_columns_to_charitable_events.rb
@@ -3,11 +3,13 @@
 class AddColumnsToCharitableEvents < ActiveRecord::Migration[7.0]
   def change
     add_column :charitable_events, :img_url, :string
+    add_column :charitable_events, :name, :string
     add_column :charitable_events, :organizer, :string
     add_column :charitable_events, :location, :string
     add_column :charitable_events, :started_at, :datetime
     add_column :charitable_events, :ended_at, :datetime
     add_column :charitable_events, :event_type, :string
+    add_column :charitable_events, :working_type, :string
     add_column :charitable_events, :bonus, :string
     add_column :charitable_events, :registration_deadline, :date
     add_column :charitable_events, :link, :string
