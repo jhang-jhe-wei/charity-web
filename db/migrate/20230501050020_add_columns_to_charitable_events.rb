40c31cafc77a48bcc2ed2cf6c803cf3ee05b631c
wells <jhang0912407249@gmail.com>
2023-05-01 13:11:56 +0800

    feat: normalize charitable_events

---
+++ db/migrate/20230501050020_add_columns_to_charitable_events.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class AddColumnsToCharitableEvents < ActiveRecord::Migration[7.0]
+  def change
+    add_column :charitable_events, :img_url, :string
+    add_column :charitable_events, :organizer, :string
+    add_column :charitable_events, :location, :string
+    add_column :charitable_events, :started_at, :datetime
+    add_column :charitable_events, :ended_at, :datetime
+    add_column :charitable_events, :event_type, :string
+    add_column :charitable_events, :bonus, :string
+    add_column :charitable_events, :registration_deadline, :date
+    add_column :charitable_events, :link, :string
+    add_column :charitable_events, :remark, :string
+    add_column :charitable_events, :source_type, :string
+  end
+end
