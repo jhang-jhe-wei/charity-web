925dbf6d887ccefc323fee12148e152c173ab52e
wells <jhang0912407249@gmail.com>
2023-05-15 02:10:46 +0800

    Refactor: refactor IgivingParser

--- lib/charity_web_parsers/igiving_parser.rb
+++ lib/charity_web_parsers/igiving_parser.rb
@@ -1,34 +1,35 @@
 # frozen_string_literal: true
 
-class IgivingParser
+require_relative 'base_parser'
+
+class IgivingParser < BaseParser
   SOURCE_TYPE = 'igiving'
   WEB_URL = 'https://www.igiving.org.tw/contents/volunteer'
 
-  attr_reader :data, :total_pages
+  attr_reader :total_pages
 
-  def initialize
-    @data = []
+  private
+
+  def pages
     doc = Nokogiri::HTML(Faraday.get(WEB_URL).body)
-    @total_pages = doc.css('#volunteer ul.pagination li:last-child a')[0][:href][/\d+/].to_i
+    total_pages = doc.css('#volunteer ul.pagination li:last-child a')[0][:href][/\d+/].to_i
+    (1..total_pages)
   end
 
-  def parse
-    (1..total_pages).each do |page|
-      url = "#{WEB_URL}?page=#{page}#main"
-      doc = Nokogiri::HTML(Faraday.get(url).body)
-      rows = doc.css('div#volunteer div.row')
-      rows.each do |row|
-        img_url = "https://www.igiving.org.tw#{row.css('img')[0]['data-original']}"
-        name = row.css('img')[0]['data-alt']
-        link = "https://www.igiving.org.tw#{row.css('a')[0]['href']}"
+  def parse_articles(page)
+    url = "#{WEB_URL}?page=#{page}#main"
+    doc = Nokogiri::HTML(Faraday.get(url).body)
+    doc.css('div#volunteer div.row')
+  end
 
-        data << {
-          img_url:,
-          name:,
-          link:
-        }
-      end
-    end
-    data
+  def parse_article(article)
+    img_url = "https://www.igiving.org.tw#{article.css('img')[0]['data-original']}"
+    name = article.css('img')[0]['data-alt']
+    link = "https://www.igiving.org.tw#{article.css('a')[0]['href']}"
+    {
+      img_url:,
+      name:,
+      link:
+    }
   end
 end
