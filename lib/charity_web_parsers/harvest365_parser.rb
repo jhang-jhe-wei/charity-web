189d42118c54ac3ab1b35699a10bf39915b3233c
wells <jhang0912407249@gmail.com>
2023-05-15 02:03:48 +0800

    Refactor: refactor Harvest365Parser

--- lib/charity_web_parsers/harvest365_parser.rb
+++ lib/charity_web_parsers/harvest365_parser.rb
@@ -1,59 +1,62 @@
 # frozen_string_literal: true
 
-class Harvest365Parser
+require_relative 'base_parser'
+
+class Harvest365Parser < BaseParser
   SOURCE_TYPE = 'harvest365'
   WEB_URL = 'https://harvest365.org/wp_function/get_activity_bysearch.php'
   DETAIL_URL = 'https://harvest365.org/wp_function/get_activity_bynum.php'
-  attr_reader :data
 
-  def initialize
-    @data = []
-  end
+  private
 
-  def parse
-    response = Faraday.post(WEB_URL, {
+  def init_parser; end
+
+  def parse_articles(page)
+    response = Faraday.post(page, {
                               area: nil,
                               eventtype: nil,
-                              startdate: nil,
-                              enddate: nil,
-                              keyword: nil
+                              startdate: nil
                             })
-    posts = JSON.parse(response.body)[1]
-    posts.each do |post|
-      img_url = "https://harvest365.org#{post['va_coverimg']}"
-      name = post['va_title']
-      time = post['va_startdate']
-      event_type = post['va_type']
-      working_type = post['va_slots']
-      viewer = nil # there is no 'viewer' key in the response
-      deadline = post['va_reg_deadline']
-      link = "https://harvest365.org/activitys/activity_content/?activity=#{post['va_seq']}"
-      bonus = "服務時數#{post['va_service_hours']}小時"
+    JSON.parse(response.body)[1]
+  end
 
-      response = Faraday.post(DETAIL_URL, {
-                                seq: post['va_seq']
-                              })
-      detail = JSON.parse(response.body)[1]
-      location = detail['va_address'] || post['va_city']
-      organizer = detail['org_name']
-      remark = detail['va_contact_detail']
+  def parse_article(article)
+    img_url = "https://harvest365.org#{article['va_coverimg']}"
+    name = article['va_title']
+    time = article['va_startdate']
+    event_type = article['va_type']
+    working_type = article['va_slots']
+    viewer = nil # there is no 'viewer' key in the response
+    deadline = article['va_reg_deadline']
+    link = "https://harvest365.org/activitys/activity_content/?activity=#{article['va_seq']}"
+    bonus = "服務時數#{article['va_service_hours']}小時"
 
-      data << {
-        img_url:,
-        name:,
-        organizer:,
-        location:,
-        time:,
-        event_type:,
-        working_type:,
-        bonus:,
-        viewer:,
-        remark:,
-        deadline:,
-        link:,
-        source_type: SOURCE_TYPE
-      }
-    end
-    data
+    response = Faraday.post(DETAIL_URL, {
+                              seq: article['va_seq']
+                            })
+    detail = JSON.parse(response.body)[1]
+    location = detail['va_address'] || article['va_city']
+    organizer = detail['org_name']
+    remark = detail['va_contact_detail']
+
+    {
+      img_url:,
+      name:,
+      organizer:,
+      location:,
+      time:,
+      event_type:,
+      working_type:,
+      bonus:,
+      viewer:,
+      remark:,
+      deadline:,
+      link:,
+      source_type: SOURCE_TYPE
+    }
+  end
+
+  def pages
+    [WEB_URL]
   end
 end
