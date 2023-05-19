914d10cbcba6dd845044a21284d13b74960bdc75
wells <jhang0912407249@gmail.com>
2023-05-20 00:28:47 +0800

    feat: add NotifiesController#callback

--- app/controllers/notifies_controller.rb
+++ app/controllers/notifies_controller.rb
@@ -6,4 +6,15 @@ class NotifiesController < ApplicationController
   end
 
   def tutorial; end
+
+  def callback
+    code = params[:code]
+    current_user = User.find_by(line_id: params[:state])
+    token = LineNotify.get_token(code)
+    if current_user.line_notify_token
+      LineNotify.revoke(current_user.line_notify_token)
+    end
+    current_user.update(line_notify_token: token)
+    LineNotify.send(token, message: '已完成連結')
+  end
 end
