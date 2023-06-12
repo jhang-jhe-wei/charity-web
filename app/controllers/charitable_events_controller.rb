3d0cd1ae46be9d14ccc8a7c4c616b1cdca40be3e
wells <jhang0912407249@gmail.com>
2023-06-12 22:56:11 +0800

    feat: add title search to search form

--- app/controllers/charitable_events_controller.rb
+++ app/controllers/charitable_events_controller.rb
@@ -20,16 +20,22 @@ class CharitableEventsController < ApplicationController
         city: params[:city] || query_params['city'],
         started_at: params[:started_at] || query_params['started_at'],
         ended_at: params[:ended_at] || query_params['ended_at'],
-        page: params[:page] || query_params['page']
+        page: params[:page] || query_params['page'],
+        name: params[:name]
       }
     end
   end
 
   def apply_filters(query)
+    filter_by_name(query[:name]) if query[:name].present?
     filter_by_city(query[:city]) if query[:city].present? && query[:city] != '全國'
     filter_by_started_at(query[:started_at]) if query[:started_at].present?
     filter_by_ended_at(query[:ended_at]) if query[:ended_at].present?
     filter_by_page(query[:page])
+  end
+
+  def filter_by_name(name)
+    @charitable_events = @charitable_events.where('name LIKE ?', "%#{name}%")
   end
 
   def filter_by_city(city)
